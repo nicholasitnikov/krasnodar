c93bbd5709292879eb7db176f5681c2b1799f272
Nikolay Sitnikov <nicholasitnikov@gmail.com>
2021-10-29 20:29:45 +0500

    fix: enable CORS

--- next.config.js
+++ next.config.js
@@ -1,16 +1,30 @@
 module.exports = {
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        source: "/*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-        ]
-      }
-    ]
+  experimental: {
+    async headers() {
+      return [
+        {
+          source: '/(.*)?',
+          headers: [
+            {
+              key: 'Access-Control-Allow-Origin',
+              value: '*'
+            },
+            {
+              key: 'Access-Control-Allow-Methods',
+              value: 'GET,HEAD,PUT,PATCH,POST,DELETE'
+            },
+          ]
+        }
+      ]
+    },
+    // temporary until https://github.com/zeit/next.js/pull/11755
+    async rewrites() {
+      return [
+        {
+          source: '/:path*',
+          destination: '/:path*'
+        }
+      ]
+    }
   }
-};
+}
